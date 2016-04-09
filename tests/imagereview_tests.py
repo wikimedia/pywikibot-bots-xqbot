5ba0c83397e44f4748d25b94228fa14183f4ae35
xqt <info@gno.de>
2016-04-09 11:58:05 +0200

    [test] Additional test for review FilePage
    Change-Id: I298b2f1e07669909fd07411e4374386e575805de

--- tests/imagereview_tests.py
+++ tests/imagereview_tests.py
@@ -14,6 +14,8 @@ import os
 import sys
 import unittest
 
+import pywikibot
+
 currentdir = os.path.dirname(
     os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
@@ -39,6 +41,71 @@ class TestMessages(unittest.TestCase):
         """Test whether messages exists."""
         self.assertTrue(hasattr(imagereview, 'msg'))
         self.assertTrue(hasattr(imagereview, 'mail_msg'))
+
+
+class TestDUP_Image(unittest.TestCase):
+
+    """Test DUP_Image class."""
+
+    REMARK = 'Urheber und Uploader sind nicht identisch.'
+    TMPL = '{{düp|Lizenz|Freigabe | Quelle| Urheber | Hinweis = %s }}' % REMARK
+
+    @classmethod
+    def setUpClass(cls):
+        cls.site = pywikibot.Site('de', 'wikipedia')
+        cls.review_tpl = pywikibot.Page(cls.site, 'düp', 10)
+
+    @classmethod
+    def tearDownClass(cls):
+        del cls.site
+        del cls.review_tpl
+
+    def tearDown(self):
+        del self.image
+
+    def init_content(self):
+        self.image = imagereview.DUP_Image(self.site, 'Sample.jpg', self.TMPL)
+        self.image._templates.append(self.review_tpl)
+        self.image.text += self.TMPL
+        self.assertEqual(self.image.text, self.image._text)
+        self.image.__init__(self.image.site, self.image.title(), self.image.text)
+        self.assertEqual(self.image._contents, self.image.text)
+
+    def test_empty_instance(self):
+        """Test instance variables"""
+        self.image = imagereview.DUP_Image(self.site, 'Sample.jpg')
+        self.assertIsNone(self.image._contents)
+        self.assertIsNone(self.image._editTime)
+        self.assertEqual(self.image._file_revisions, dict())
+        self.assertEqual(self.image._revisions, dict())
+        self.assertIsNone(self.image.done)
+        self.assertFalse(self.image.info)
+        self.assertEqual(self.image.reasons, set([]))
+        self.assertIsNone(self.image.remark)
+        self.assertEqual(self.image.review_tpl, list())
+
+    def test_instance_with_content(self):
+        """Test instance variables with content given."""
+        self.init_content()
+        self.assertIsNone(self.image._editTime)
+        self.assertFalse(self.image.done)
+        self.assertTrue(self.image.info)
+        self.assertEqual(len(self.image.reasons), 5)
+        self.assertIsNone(self.image.remark)
+        self.assertEqual(self.image.review_tpl[0], self.review_tpl)
+
+    def test_valid_reasons(self):
+        """Test validReasons method."""
+        self.init_content()
+        self.assertTrue(self.image.validReasons)
+        self.assertEqual(self.image.remark, self.REMARK)
+        self.assertLessEqual(self.image.reasons, set(imagereview.DUP_REASONS))
+
+    @unittest.expectedFailure
+    def test_hasRefs(self):
+        """Test hasRefs method."""
+        self.init_content()
+        self.assertTrue(self.image.hasRefs)
 
 
 if __name__ == '__main__':
