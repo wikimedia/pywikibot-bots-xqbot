ee301672a584a9c965a5ff7936cd6e8fb7d4ff73
xqt <info@gno.de>
2024-08-30 16:23:34 +0200

    [PEP8] fix flake8 issues
    Change-Id: Ib71f35294e27fbdff59520f68a7f67a461df3990

--- bot-stats.py
+++ bot-stats.py
@@ -1,76 +1,85 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
-import sys              # To not have wikipedia and this in one dir we'll import sys
-import re               # Used for regular expressions
-import pywikibot        # pywikibot framework
+"""Update bot statistic."""
+#
+# (C) Euku, ?-2024
+# (C) xqt, 2024
+#
+from __future__ import annotations
+
 import operator
+import re               # Used for regular expressions
+from datetime import datetime
+import dateutil.parser
+
+import pywikibot        # pywikibot framework
 import pywikibot.exceptions
 from pywikibot.data import api
 from pywikibot import textlib
-from datetime import datetime, date, timedelta
-from time import localtime, strftime, mktime    # strftime-Function and related
-import dateutil.parser
-import time
 
-botStatusInactiveThreshold = 3*356/12
-mysite = pywikibot.Site()  # ohne Parameter, falls de-wiki default-Einstellung ist
+mysite = pywikibot.Site()  # ohne Parameter, falls de-wiki default-Einstellung
 
-formerBotNames = ["ArchivBot", "LinkFA-Bot", "RevoBot", "MerlBot", "KLBot2", "Luckas-bot", "Sebbot", "Beitragszahlen", "CopperBot", "ZéroBot", "TXiKiBoT", "Thijs!bot", "MerlIwBot"]
+former_botnames = [
+    'ArchivBot', 'LinkFA-Bot', 'RevoBot', 'MerlBot', 'KLBot2', 'Luckas-bot',
+    'Sebbot', 'Beitragszahlen', 'CopperBot', 'ZéroBot', 'TXiKiBoT',
+    'Thijs!bot', 'MerlIwBot',
+]
 
-userDic = {}
-userDic["AkaBot"] = "2004-11-30"
-userDic["ApeBot"] = "2003-07-30"
-userDic["BWBot"] = "2004-08-10"
-userDic["Bota47"] = "2005-07-19"
-userDic["Botteler"] = "2004-08-23"
-userDic["Chlewbot"] = "2005-11-30"
-userDic["Chobot"] = "2005-06-18"
-userDic["ConBot"] = "2004-10-26"
-userDic["FlaBot"] = "2004-11-21"
-userDic["GeoBot"] = "2005-07-15"
-userDic["Gpvosbot"] = "2005-06-11"
-userDic["KocjoBot"] = "2005-10-08"
-userDic["LeonardoRob0t"] = "2004-11-30"
-userDic["MelancholieBot"] = "2005-09-22"
-userDic["PortalBot"] = "2005-11-01"
-userDic["PyBot"] = "2003-05-28"
-userDic["RKBot"] = "2005-04-10"
-userDic["RedBot"] = "2005-01-21"
-userDic["Robbot"] = "2003-10-11"
-userDic["RobotE"] = "2005-05-20"
-userDic["RobotQuistnix"] = "2005-07-17"
-userDic["Sk-Bot"] = "2004-10-20"
-userDic["SpBot"] = "2005-10-06"
-userDic["Tasca.bot"] = "2005-07-30"
-userDic["Tsca.bot"] = "2005-07-30"
-userDic["YurikBot"] = "2005-07-31"
-userDic["Zwobot"] = "2003-12-02"
+userdict = {}
+userdict['AkaBot'] = '2004-11-30'
+userdict['ApeBot'] = '2003-07-30'
+userdict['BWBot'] = '2004-08-10'
+userdict['Bota47'] = '2005-07-19'
+userdict['Botteler'] = '2004-08-23'
+userdict['Chlewbot'] = '2005-11-30'
+userdict['Chobot'] = '2005-06-18'
+userdict['ConBot'] = '2004-10-26'
+userdict['FlaBot'] = '2004-11-21'
+userdict['GeoBot'] = '2005-07-15'
+userdict['Gpvosbot'] = '2005-06-11'
+userdict['KocjoBot'] = '2005-10-08'
+userdict['LeonardoRob0t'] = '2004-11-30'
+userdict['MelancholieBot'] = '2005-09-22'
+userdict['PortalBot'] = '2005-11-01'
+userdict['PyBot'] = '2003-05-28'
+userdict['RKBot'] = '2005-04-10'
+userdict['RedBot'] = '2005-01-21'
+userdict['Robbot'] = '2003-10-11'
+userdict['RobotE'] = '2005-05-20'
+userdict['RobotQuistnix'] = '2005-07-17'
+userdict['Sk-Bot'] = '2004-10-20'
+userdict['SpBot'] = '2005-10-06'
+userdict['Tasca.bot'] = '2005-07-30'
+userdict['Tsca.bot'] = '2005-07-30'
+userdict['YurikBot'] = '2005-07-31'
+userdict['Zwobot'] = '2003-12-02'
 
-def queryLastEdit(username):
-	# load user contribs from API
-        req = api.Request(site=mysite, action="query")
-        req['list'] = 'usercontribs'
-        req['ucuser'] = username
-        req['ucdir'] = 'older'
-        req['uclimit'] = 1
-        req['rawcontinue'] = ''
-        data = req.submit()
-        try:
-            for x in data['query']['usercontribs']:
-               return x['timestamp'] # last edit
-        except:
-            return None 
 
-if __name__ == "__main__":
-    formerBotList = []
-    req1 = api.Request(site=mysite, action="query")
+def query_last_edit(username):
+    """Load user contribs from API."""
+    req = api.Request(site=mysite, action='query')
+    req['list'] = 'usercontribs'
+    req['ucuser'] = username
+    req['ucdir'] = 'older'
+    req['uclimit'] = 1
+    req['rawcontinue'] = ''
+    data = req.submit()
+    try:
+        for x in data['query']['usercontribs']:
+            return x['timestamp']  # last edit
+    except Exception:
+        pywikibot.exception()
+    return None
+
+
+if __name__ == '__main__':
+    req1 = api.Request(site=mysite, action='query')
     req1['list'] = 'users'
-    req1['ususers'] = "|".join(str(x) for x in formerBotNames)
+    req1['ususers'] = '|'.join(str(x) for x in former_botnames)
     req1['usprop'] = 'editcount|registration'
     data1 = req1.submit()
 
-    botList = []
-    req2 = api.Request(site=mysite, action="query")
+    botlist = []
+    req2 = api.Request(site=mysite, action='query')
     req2['list'] = 'allusers'
     req2['augroup'] = 'bot'
     req2['aulimit'] = 'max'
@@ -79,48 +88,54 @@ if __name__ == "__main__":
     for x in data1['query']['users'] + data2['query']['allusers']:
         p1 = re.compile(r'(?P<date>\d\d\d\d\-\d\d\-\d\d).+', re.UNICODE)
         matches1 = p1.finditer(x['registration'])
-        reg = "?"
+        reg = '?'
         for match1 in matches1:
             reg = match1.group('date')
-        if reg == "?" or reg == datetime.today().strftime("%Y-%m-%d"):
-            if x['name'] in userDic:
-               reg = userDic[x['name']] + "*"
+        if reg == '?' or reg == datetime.today().strftime('%Y-%m-%d'):
+            if x['name'] in userdict:
+                reg = userdict[x['name']] + '*'
             else:
-               reg = "?"
-        botList.append((textlib.replaceExcept(x['name'], "&amp;", "&", []), x['editcount'], reg))
-    
-    botList = reversed(sorted(botList, key=operator.itemgetter(1)))
-    pageText = '{{Benutzer:Euku/B:Navigation}}\n\
+                reg = '?'
+        botlist.append((textlib.replaceExcept(x['name'], '&amp;', '&', []),
+                        x['editcount'], reg))
+
+    botlist = sorted(botlist, key=operator.itemgetter(1), reverse=True)
+    pagetext = "{{Benutzer:Euku/B:Navigation}}\n\
 Aufgeführt sind alle Bots die einen Bot-Flag besitzen. Stand: ~~~~~<br />Ein Bot gilt als inaktiv, wenn er in den letzten drei Monaten keinen Beitrag geleistet hat.\n\n\
 [//de.wikipedia.org/w/index.php?title=Benutzer:Euku/Botstatistik&diff=curr&oldid=prev&diffonly=1 &Auml;nderungen der letzten Woche]\n\
-{|class="sortable wikitable"\n\
-! #\n! Botname\n!Beiträge\n! Gesamtbearbeitungen\n! Letzte Bearbeitung\n! Anmeldedatum\n'
+{|class='sortable wikitable'\n\
+! #\n! botname\n!Beiträge\n! Gesamtbearbeitungen\n! Letzte Bearbeitung\n! Anmeldedatum\n"
     counter = 0
-    allEdits = 0
+    all_edits = 0
     now = datetime.now()
-    for bot in botList:
+    for bot in botlist:
         counter += 1
-        botName, botEditCounter, botCreationDate = bot
-        allEdits += botEditCounter
-        remark = ""
-        lastEditRes = queryLastEdit(botName)
-        if (lastEditRes != None):
-            lastEdit = dateutil.parser.parse(lastEditRes).replace(tzinfo=None)
-            if botName in formerBotNames:
-                remark = "(ehemalig)"
-            elif (now - lastEdit).days > 3*30:
-                remark = "(inaktiv)"
-            pageText += "|-\n|%s||[[Benutzer:%s|%s]] %s||[[Spezial:Beiträge/%s|B]]||%s||%s||%s\n" % (counter, botName, botName, remark, botName, botEditCounter, lastEdit.strftime("%Y-%m-%d"), botCreationDate)
+        botname, bot_editcounter, bot_creationdate = bot
+        all_edits += bot_editcounter
+        remark = ''
+        last_edit_res = query_last_edit(botname)
+        if last_edit_res is not None:
+            last_edit = dateutil.parser.parse(last_edit_res).replace(tzinfo=None)
+            if botname in former_botnames:
+                remark = '(ehemalig)'
+            elif (now - last_edit).days > 3 * 30:
+                remark = '(inaktiv)'
+            pagetext += '|-\n|%s||[[Benutzer:%s|%s]] %s||[[Spezial:Beiträge/%s|B]]||%s||%s||%s\n' % (
+                counter, botname, botname, remark, botname, bot_editcounter,
+                last_edit.strftime('%Y-%m-%d'), bot_creationdate)
         else:
-            if botName in formerBotNames:
-                remark = "(ehemalig)"
-            pageText += "|-\n|%s||[[Benutzer:%s|%s]] %s||[[Spezial:Beiträge/%s|B]]||%s||-||%s\n" % (counter, botName, botName, remark, botName, botEditCounter, botCreationDate)
-    
-    pageText += "|}\nGesamtbearbeitungen durch diese Bots: %s ([[Benutzer_Diskussion:Euku/Botstatistik#Bots_ohne_einen_Edit_mit_einem_letzten_Edit|eine Schätzung]])<br />\n<nowiki>*</nowiki> = Datum der ersten Bearbeitung<br/>\nehemalig = das Benutzterkonto besitzt kein Botflag mehr" % "{:,}".format(allEdits).replace(',', '.')
-    
+            if botname in former_botnames:
+                remark = '(ehemalig)'
+            pagetext += '|-\n|%s||[[Benutzer:%s|%s]] %s||[[Spezial:Beiträge/%s|B]]||%s||-||%s\n' % (
+                counter, botname, botname, remark, botname, bot_editcounter,
+                bot_creationdate)
+
+    pagetext += '|}\nGesamtbearbeitungen durch diese Bots: %s ([[Benutzer_Diskussion:Euku/Botstatistik#Bots_ohne_einen_Edit_mit_einem_letzten_Edit|eine Schätzung]])<br />\n<nowiki>*</nowiki> = Datum der ersten Bearbeitung<br/>\nehemalig = das Benutzterkonto besitzt kein Botflag mehr' % '{:,}'.format(
+        all_edits).replace(',', '.')
+
     # print pageText use pywikibot.out
     # save it
-    pywikibot.output("Speichere...")
-    pageOp = pywikibot.Page(pywikibot.Site(), "Benutzer:Euku/Botstatistik")
-    pywikibot.output(pageText)
-    pageOp.put(pageText, "Update", show_diff=True)
+    pywikibot.output('Speichere...')
+    page_op = pywikibot.Page(mysite, 'Benutzer:Euku/Botstatistik')
+    pywikibot.output(pagetext)
+    page_op.put(pagetext, 'Update', show_diff=True)
