33489e96b7d4ffff78bc75dc7b99c4cad066ea96
xqt <info@gno.de>
2024-08-31 17:18:59 +0200

    [IMPR] Add status row
    - create userdict directly withour assingments
    - use pywikibot.Timestamp for timestamp handling
    - add a status row
    - use groups to check for older bots
    - update page footer
    
    Change-Id: I4f621a0cffdd42837d164a1a8a27791727f8c209

--- bot-stats.py
+++ bot-stats.py
@@ -14,13 +14,11 @@ The following parameters are supported:
 from __future__ import annotations
 
 import operator
-import re               # Used for regular expressions
-import dateutil.parser
 from contextlib import suppress
-from datetime import datetime
 from itertools import chain
 
-import pywikibot        # pywikibot framework
+import pywikibot  # pywikibot framework
+from pywikibot import Timestamp
 from pywikibot.backports import Generator, removeprefix
 from pywikibot.bot import CurrentPageBot, SingleSiteBot
 
@@ -30,34 +28,36 @@ former_botnames = {
     'Thijs!bot', 'MerlIwBot',
 }
 
-userdict = {}
-userdict['AkaBot'] = '2004-11-30'
-userdict['ApeBot'] = '2003-07-30'
-userdict['BWBot'] = '2004-08-10'
-userdict['Bota47'] = '2005-07-19'
-userdict['Botteler'] = '2004-08-23'
-userdict['Chlewbot'] = '2005-11-30'
-userdict['Chobot'] = '2005-06-18'
-userdict['ConBot'] = '2004-10-26'
-userdict['FlaBot'] = '2004-11-21'
-userdict['GeoBot'] = '2005-07-15'
-userdict['Gpvosbot'] = '2005-06-11'
-userdict['KocjoBot'] = '2005-10-08'
-userdict['LeonardoRob0t'] = '2004-11-30'
-userdict['MelancholieBot'] = '2005-09-22'
-userdict['PortalBot'] = '2005-11-01'
-userdict['PyBot'] = '2003-05-28'
-userdict['RKBot'] = '2005-04-10'
-userdict['RedBot'] = '2005-01-21'
-userdict['Robbot'] = '2003-10-11'
-userdict['RobotE'] = '2005-05-20'
-userdict['RobotQuistnix'] = '2005-07-17'
-userdict['Sk-Bot'] = '2004-10-20'
-userdict['SpBot'] = '2005-10-06'
-userdict['Tasca.bot'] = '2005-07-30'
-userdict['Tsca.bot'] = '2005-07-30'
-userdict['YurikBot'] = '2005-07-31'
-userdict['Zwobot'] = '2003-12-02'
+# unknown creation date; use first edit timestamp instead
+userdict = {
+    'AkaBot': '2004-11-30',
+    'ApeBot': '2003-07-30',
+    'BWBot': '2004-08-10',
+    'Bota47': '2005-07-19',
+    'Botteler': '2004-08-23',
+    'Chlewbot': '2005-11-30',
+    'Chobot': '2005-06-18',
+    'ConBot': '2004-10-26',
+    'FlaBot': '2004-11-21',
+    'GeoBot': '2005-07-15',
+    'Gpvosbot': '2005-06-11',
+    'KocjoBot': '2005-10-08',
+    'LeonardoRob0t': '2004-11-30',
+    'MelancholieBot': '2005-09-22',
+    'PortalBot': '2005-11-01',
+    'PyBot': '2003-05-28',
+    'RKBot': '2005-04-10',
+    'RedBot': '2005-01-21',
+    'Robbot': '2003-10-11',
+    'RobotE': '2005-05-20',
+    'RobotQuistnix': '2005-07-17',
+    'Sk-Bot': '2004-10-20',
+    'SpBot': '2005-10-06',
+    'Tasca.bot': '2005-07-30',
+    'Tsca.bot': '2005-07-30',
+    'YurikBot': '2005-07-31',
+    'Zwobot': '2003-12-02',
+}
 
 
 class BotStatsUpdater(SingleSiteBot, CurrentPageBot):
@@ -112,7 +112,8 @@ class BotStatsUpdater(SingleSiteBot, CurrentPageBot):
         )
 
         page_footer = (
-            '([[Benutzer_Diskussion:Euku/Botstatistik'
+            '([[Wikipedia_Diskussion:'
+            'Liste der Bots nach Anzahl der Bearbeitungen'
             '#Bots_ohne_einen_Edit_mit_einem_letzten_Edit|eine Schätzung]])'
             '<br />\n<nowiki>*</nowiki> = Datum der ersten Bearbeitung<br/>\n'
             'ehemalig = das Benutzterkonto besitzt kein Botflag mehr\n\n'
@@ -123,6 +124,7 @@ class BotStatsUpdater(SingleSiteBot, CurrentPageBot):
 {|class="sortable wikitable"
 ! #
 ! Botname
+! Status
 ! Beiträge
 ! Gesamtbearbeitungen
 ! Letzte Bearbeitung
@@ -140,63 +142,61 @@ class BotStatsUpdater(SingleSiteBot, CurrentPageBot):
         """Collect bots data and create table content."""
         pywikibot.info('collecting data...')
         botlist = []
-        data1 = self.site.users(self.former_botnames())
-        data2 = self.site.allusers(group='bot')
+        data1 = self.site.allusers(group='bot')
+        data2 = self.site.users(self.former_botnames())
         data3 = self.site.users(former_botnames)
 
         pywikibot.info('\ncreating botlist...')
-        p1 = re.compile(r'(?P<date>\d\d\d\d\-\d\d\-\d\d).+')
         seen = set()
         # Use bot users first in the chain.
         # The bot flag can have been granted for former botnames
-        for x in chain(data2, data1, data3):
+        for x in chain(data1, data2, data3):
             if x['name'] in seen:
                 continue
             seen.add(x['name'])
 
-            with suppress(TypeError):  # x['registration'] is None
-                matches1 = p1.finditer(x['registration'])
-            reg = '?'
-            for match1 in matches1:
-                reg = match1.group('date')
-            if reg == '?' or reg == datetime.today().strftime('%Y-%m-%d'):
-                if x['name'] in userdict:
-                    reg = userdict[x['name']] + '*'
-                else:
-                    reg = '?'
+            try:
+                ts = Timestamp.fromISOformat(x['registration'])
+                suffix = ''
+            except (TypeError, ValueError):
+                ts = Timestamp.fromisoformat(userdict[x['name']])
+                suffix = '*'
+
             botlist.append((x['name'].replace('&amp;', '&'),
-                            x['editcount'], reg))
+                            x['editcount'],
+                            ts, suffix,
+                            x['groups']))
 
         pywikibot.info('creating wiki table...', newline=False)
         botlist = sorted(botlist, key=operator.itemgetter(1), reverse=True)
         pagetext = ''
         all_edits = 0
-        now = datetime.now()
+        now = Timestamp.now()
         for bot in botlist:
             self.counter['collect'] += 1
             if self.counter['collect'] % 10 == 0:
                 pywikibot.info('.', newline=False)
-            botname, bot_editcounter, bot_creationdate = bot
+            botname, bot_editcounter, bot_creationdate, suffix, groups = bot
             all_edits += bot_editcounter
-            remark = ''
+            remark = 'aktiv'
             last_edit_res = self.query_last_edit(botname)
-            if last_edit_res is not None:
-                last_edit = dateutil.parser.parse(
-                    last_edit_res).replace(tzinfo=None)
-                if (now - last_edit).days > 3 * 30:
-                    remark = '(inaktiv)'
-                last_edit_str = last_edit.strftime('%Y-%m-%d')
-            else:
+            if last_edit_res is None:
                 last_edit_str = '-'
-            if botname in former_botnames:
-                remark = '(ehemalig)'
+            else:
+                last_edit = Timestamp.fromISOformat(last_edit_res)
+                if (now - last_edit).days > 3 * 30:
+                    remark = 'inaktiv'
+                last_edit_str = str(last_edit)[:10]
+            if 'bot' not in groups:
+                remark = 'ehemalig'
             pagetext += (
                 f'|-\n|{self.counter["collect"]}|'
-                f'|[[Benutzer:{botname}|{botname}]] {remark}|'
+                f'|[[Benutzer:{botname}|{botname}]]|'
+                f'|{remark}|'
                 f'|[[Spezial:Beiträge/{botname}|B]]|'
                 f'|{bot_editcounter}|'
                 f'|{last_edit_str}|'
-                f'|{bot_creationdate}\n'
+                f'|{str(bot_creationdate)[:10]}{suffix}\n'
             )
 
         pywikibot.info()
